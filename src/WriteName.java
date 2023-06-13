1137c8b2ee960d1eb16843c0b2fc82c2a71fcb0c
chojangg <s2115@e-mirim.hs.kr>
2023-06-13 10:12:05 +0900

    이름 입력받기 수정중

--- src/WriteName.java
+++ src/WriteName.java
@@ -1,27 +1,55 @@
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.io.File;
+import java.io.IOException;
 
 public class WriteName extends JFrame {
     public WriteName() {
+        int width = 1920;    // 프레임 가로
+        int height = 1080;   // 프레임 높이
+        String yourname;         // 이름
         // 이미지 파일 경로
-        String ruleimagePath = "src/image/name.png";
+        String ruleImagePath = "src/image/name.png";
 
         // 창 생성
         JFrame frame = new JFrame("Write Your Name");
-        frame.setSize(1920, 1080);
-//        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);  // 전체 화면으로 설정
+        frame.setSize(width, height);
+        // frame.setExtendedState(JFrame.MAXIMIZED_BOTH);  // 전체 화면으로 설정
         frame.setUndecorated(true);  // 타이틀 바 숨김
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // 이미지 패널 생성
-        ImagePanel imagePanel = new ImagePanel(ruleimagePath);
+        ImagePanel imagePanel = new ImagePanel(ruleImagePath);
         frame.add(imagePanel);
 
-        // 이미지 패널에 버튼 추가 및 정렬 설정
-        imagePanel.setLayout(new GridBagLayout());
+        // 이미지 패널에 텍스트를 표시할 JLabel 생성
+        JLabel label = new JLabel("이름을 입력해주세요.");
+        label.setForeground(Color.BLACK);
+
+        // 마진 생성 및 설정
+        int margin = 200;  // 원하는 마진 크기 설정
+        EmptyBorder marginBorder = new EmptyBorder(margin, margin, margin, margin);
+        label.setBorder(marginBorder);
+
+        // 폰트 설정
+        Font koreanFont = null;
+        try {
+            koreanFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/font/font.ttf")).deriveFont(Font.PLAIN, 80);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(koreanFont);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+
+        if (koreanFont != null) {
+            label.setFont(koreanFont);
+        }
+
+        // 이미지 패널에 JLabel 추가
+        imagePanel.add(label);
 
         // 창 표시
         frame.setVisible(true);
@@ -37,6 +65,10 @@ public class WriteName extends JFrame {
     }
 
     public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog(null, "이름을 입력하세요:", "이름 입력", JOptionPane.PLAIN_MESSAGE);
+        if (name != null && !name.isEmpty()) {
+            System.out.println("입력된 이름: " + name);
+        }
         SwingUtilities.invokeLater(() -> new WriteName());
     }
 }
---
+++ src/WriteName2.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.Font;
+import java.io.File;
+
+public class WriteName2 extends JFrame {
+
+    public WriteName2() {
+        try {
+            Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/font/font.ttf")); // 폰트 파일 경로를 적절히 수정
+            UIManager.put("OptionPane.messageFont", customFont.deriveFont(Font.PLAIN, 12));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String name = JOptionPane.showInputDialog(null, "이름을 입력해주세요.", "Write Your Name", JOptionPane.PLAIN_MESSAGE);
+        if (name != null && !name.isEmpty()) {
+            System.out.println("입력된 이름: " + name);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new WriteName2());
+    }
+}
