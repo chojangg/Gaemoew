4c9b9b761cd9b8103b4066c733f54aa67a32574a
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-06-19 10:45:41 +0900

    Story 화면 사이즈 조정

--- src/Story.java
+++ src/Story.java
@@ -5,53 +5,69 @@ import java.awt.event.ActionListener;
 
 public class Story {
     public static void main(String[] args) {
-        // Create a JFrame to hold the GIF
+        // GIF를 보여줄 JFrame 생성
         JFrame frame = new JFrame();
-        frame.setUndecorated(true);  // 타이틀 바 숨김
+        frame.setSize(1920, 1080);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-        // Get the default graphics device
+        // 기본 그래픽 디바이스 가져오기
         GraphicsDevice graphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 
-        // Set the frame size to match the screen size
+        // 프레임 크기를 화면 크기와 동일하게 설정
         frame.setSize(graphicsDevice.getDisplayMode().getWidth(), graphicsDevice.getDisplayMode().getHeight());
 
-        // Set the frame position to (0, 0)
+        // 프레임 위치를 (0, 0)으로 설정
         frame.setLocation(0, 0);
 
-        // Create an ImageIcon from the GIF file
-        ImageIcon icon = new ImageIcon("src/image/story_text.png");
+        // 커스텀 패널 생성
+        CustomPanel panel = new CustomPanel("src/image/story_text.png");
 
-        // Create a JLabel to display the GIF
-        JLabel label = new JLabel(icon);
+        // 커스텀 패널 크기를 프레임 크기와 동일하게 설정
+        panel.setSize(frame.getSize());
 
-        // Add the label to the frame's content pane
-        frame.getContentPane().add(label);
+        // 커스텀 패널의 레이아웃을 중앙 정렬로 설정하여 배경 이미지를 가운데로 정렬
+        panel.setLayout(new BorderLayout());
 
-        // Set the label's layout to center align the GIF
-        label.setHorizontalAlignment(JLabel.CENTER);
-
-        // Create a Timer to close the frame after a certain delay
-        int gifDuration = 4000; // GIF duration in milliseconds (4 seconds)
+        // 일정 시간 후에 프레임을 닫고 Play 화면 시작
+        int gifDuration = 4000; // GIF 지속 시간 (4초)
         Timer timer = new Timer(gifDuration, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Start the Play screen
-                WriteName2.main(new String[0]);
-                // Close the frame
+                // 프레임 닫기
                 frame.dispose();
+                // Play 화면 시작
+                WriteName2.main(new String[0]);
             }
         });
 
-        // Start the timer
+        // 타이머 시작
         timer.setRepeats(false);
         timer.start();
 
-        // Set the frame to full screen
+        // 프레임을 전체 화면으로 설정
         graphicsDevice.setFullScreenWindow(frame);
 
-        // Show the frame
+        // 커스텀 패널을 프레임의 콘텐트 패널로 설정
+        frame.setContentPane(panel);
+
+        // 프레임 표시
         frame.setVisible(true);
+    }
+
+    static class CustomPanel extends JPanel {
+        private Image backgroundImage;
+
+        public CustomPanel(String imagePath) {
+            // 배경 이미지 로드
+            ImageIcon icon = new ImageIcon(imagePath);
+            backgroundImage = icon.getImage();
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            // 배경 이미지 그리기
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
     }
 }
