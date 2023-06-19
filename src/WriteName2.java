1907d55abf2db0b48b447f9cec3fc2b562f35408
steam88ys <yssteam88@gmail.com>
2023-06-19 20:35:40 +0900

    이름 입력창 구현

--- src/WriteName2.java
+++ src/WriteName2.java
@@ -1,45 +1,92 @@
 import javax.swing.*;
-import java.awt.Font;
+import java.awt.*;
+import java.awt.FontFormatException;
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class WriteName2 extends JFrame {
+public class WriteName2 extends JDialog {
 
     private static final String DB_URL = "jdbc:mysql://localhost:3308/sys"; // 데이터베이스 URL
     private static final String DB_USER = "root"; // 데이터베이스 사용자 이름
     private static final String DB_PASSWORD = "alflarhkgkrrh1!"; // 데이터베이스 비밀번호
 
     public WriteName2() {
+        // Font 설정
+        Font font = null;
         try {
-            Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/font/font.ttf")); // 폰트 파일 경로를 적절히 수정
-            UIManager.put("OptionPane.messageFont", customFont.deriveFont(Font.PLAIN, 12));
+            File fontFile = new File("src/font/font.ttf");
+            font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        String name = JOptionPane.showInputDialog(null, "이름을 입력해주세요.", "Write Your Name", JOptionPane.PLAIN_MESSAGE);
-        if (name != null && !name.isEmpty()) {
-            System.out.println("입력된 이름: " + name);
-            insertNameToDatabase(name); // 데이터베이스에 이름 삽입
-        } else {
-            System.out.println("이름이 입력되지 않았습니다.");
-        }
+        JTextField nameField = new JTextField();
+        nameField.setColumns(20); // 입력 필드의 칼럼 수 설정
+        nameField.setForeground(Color.darkGray);
+        nameField.setFont(font.deriveFont(Font.PLAIN, 40)); // 입력하는 글씨의 크기 설정
 
-        if (name != null) { // 사용자가 입력을 취소하지 않았을 경우
-            if (!name.isEmpty()) {  // 이름이 비어있지 않은 경우
-                // Play.java 실행
-                Play.main(new String[]{});
-            } else {    // 이름이 비어있는 경우
-                // Main.java 실행
-                Main.main(new String[]{});
+        JPanel panel = new JPanel(new GridBagLayout()) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                ImageIcon background = new ImageIcon("src/image/name.png"); // 배경 이미지 파일 경로를 적절히 수정
+
+                // 패널의 크기에 맞게 이미지 크기를 조정
+                Image image = background.getImage();
+                Image scaledImage = image.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
+                background = new ImageIcon(scaledImage);
+
+                g.drawImage(background.getImage(), 0, 0, getWidth(), getHeight(), this);
             }
-        } else {    // 사용자가 입력을 취소한 경우
-            // Main.java 실행
-            Main.main(new String[]{});
-        }
+        };
+
+        // 현재 사용자의 화면 크기로 패널 크기 설정
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        panel.setPreferredSize(screenSize);
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.insets = new Insets(20, 20, 20, 20);
+
+        JLabel nameLabel = new JLabel("이름을 입력해주세요.");
+        nameLabel.setFont(font.deriveFont(Font.PLAIN, 45));
+        panel.add(nameLabel, constraints);
+
+        constraints.gridy = 1;
+        panel.add(nameField, constraints);
+
+        setTitle("Write Your Name");
+        setUndecorated(true); // 타이틀 바 숨기기
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setContentPane(panel);
+
+        setBounds(0, 0, screenSize.width, screenSize.height); // 전체 화면으로 설정
+
+        setVisible(true); // 다이얼로그를 표시
+
+        nameField.addActionListener(e -> {
+            String name = nameField.getText();
+            if (!name.isEmpty()) {
+                System.out.println("입력된 이름: " + name);
+                insertNameToDatabase(name); // 데이터베이스에 이름 삽입
+            } else {
+                System.out.println("이름이 입력되지 않았습니다.");
+            }
+
+            Main.main(new String[]{}); // Main.java 파일 실행
+            dispose(); // 현재 창 닫기
+        });
     }
 
     private void insertNameToDatabase(String name) {
@@ -56,6 +103,9 @@ public class WriteName2 extends JFrame {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new WriteName2());
+        SwingUtilities.invokeLater(() -> {
+            WriteName2 writeName2 = new WriteName2();
+            writeName2.setVisible(true); // 다이얼로그를 표시
+        });
     }
 }
